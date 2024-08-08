563945b9e6efdf98db82e1615a3d4fe0f34291b3
Thet Hnin <thetthet4462718@gmail.com>
2024-08-08 21:34:16 +0630

    drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,244 @@
 "use client";
+import * as React from "react";
+import { styled, useTheme, Theme, CSSObject } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Face4Icon from "@mui/icons-material/Face4";
+import Face3Icon from "@mui/icons-material/Face3";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import { Colors } from "@/const/colors";
 
-import React from "react";
+const drawerWidth = 240;
 
-function BookSearch() {
-  return <div>BookSearch</div>;
+const drawer_list = [
+  {
+    title: "Dashboard",
+    icon: <DashboardIcon sx={{ color: Colors.white }} />,
+    routeName: "/",
+  },
+  {
+    title: "Teacher",
+    icon: <Face4Icon sx={{ color: Colors.white }} />,
+    routeName: "/teacher",
+  },
+  {
+    title: "Student",
+    icon: <Face3Icon sx={{ color: Colors.white }} />,
+    routeName: "/student",
+  },
+  {
+    title: "Book",
+    icon: <AutoStoriesIcon sx={{ color: Colors.white }} />,
+    routeName: "/book",
+  },
+];
+
+const openedMixin = (theme: Theme): CSSObject => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+  open?: boolean;
 }
 
-export default BookSearch;
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})<AppBarProps>(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+interface AppProps {
+  children: React.ReactNode;
+}
+
+const Page: React.FC<AppProps> = ({ children }) => {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const [selectedItem, setSelectedItem] = React.useState("");
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar sx={{ backgroundColor: Colors.primary_color }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 5,
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Library Management System
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <DrawerHeader sx={{ backgroundColor: Colors.primary_color }}>
+          <div
+            style={{
+              display: "flex",
+              flex: 1,
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src="http://www.tumaubin.edu.mm/wp-content/uploads/2019/01/cropped-222222222222-1-1444x1536.png"
+              style={{ width: 40, height: 45 }}
+            />
+            <Typography
+              style={{ fontWeight: "bold", color: Colors.white, marginLeft: 8 }}
+            >
+              TU (Maubin)
+            </Typography>
+          </div>
+
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon sx={{ color: Colors.white }} />
+            ) : (
+              <ChevronLeftIcon sx={{ color: Colors.white }} />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List sx={{ backgroundColor: Colors.primary_color, height: "100vh" }}>
+          {drawer_list?.map((item, index) => (
+            <ListItem
+              key={index}
+              disablePadding
+              sx={{ display: "block" }}
+              onClick={() => setSelectedItem(item?.title)}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? "initial" : "center",
+                  px: 2.5,
+                }}
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : "auto",
+                    justifyContent: "center",
+                  }}
+                >
+                  {item?.icon}
+                </ListItemIcon>
+                <ListItemText
+                  primary={item?.title}
+                  sx={{
+                    opacity: open ? 1 : 0,
+                    color:
+                      selectedItem === item?.title
+                        ? Colors.secondary_color
+                        : Colors.white,
+                  }}
+                />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          height: "100vh",
+        }}
+      >
+        <DrawerHeader />
+        {children}
+      </Box>
+    </Box>
+  );
+};
+export default Page;
---
+++ src/const/colors.ts
@@ -0,0 +1,5 @@
+export const Colors = {
+    primary_color: '#294A70',
+    secondary_color: '#6EACDA',
+    white: '#ffffff'
+}
