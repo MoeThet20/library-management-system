3cce8113ace1a74425f1906aceded06fcf609e3e
MoeThet20 <moethet.nice@gmail.com>
2024-08-29 01:33:58 +0700

    [ add ] pwa config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import withPWA from "next-pwa";
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   env: {
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
@@ -8,4 +10,9 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Library Management System",
   description: "LMS",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "next14", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    {
+      name: "khine zaw htet",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
 export default function RootLayout({
